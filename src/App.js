ae08241bce2fb4eba1803176de521b9a2b33ef09
Diana Viglucci <dvigl86@gmail.com>
2022-05-13 09:17:45 -0400

    can retrieve basic data from api and display in browser

--- src/App.js
+++ src/App.js
@@ -1,25 +1,61 @@
-import logo from './logo.svg';
 import './App.css';
+import { useState } from 'react';
 
-function App() {
+const App = () => {
+  const [inputText, setInputText] = useState('');
+  const [outputText, setOutputText] = useState('');
+
+  const requestData = {
+    prompt: inputText,
+    temperature: 0.5,
+    max_tokens: 64,
+    top_p: 1.0,
+    frequency_penalty: 0.0,
+    presence_penalty: 0.0,
+  };
+
+  const updateInputText = (event) => {
+    console.log('updating input to >>>', event.target.value)
+    setInputText(event.target.value)
+  }
+
+  async function onSubmit(event) {
+    event.preventDefault();
+    console.log('about to fetch...')
+    console.log('fucking key >>', process.env.REACT_APP_OPENAI_API_KEY)
+    const response = await fetch(
+      'https://api.openai.com/v1/engines/text-curie-001/completions',
+      {
+        method: 'POST',
+        headers: {
+          'Content-Type': 'application/json',
+          Authorization: `Bearer ${process.env.REACT_APP_OPENAI_API_KEY}`,
+        },
+        body: JSON.stringify(requestData),
+      }
+    );
+    const data = await response.json();
+    console.log('data >>>', data)
+    console.log('data.result ...', data.choices[0].text)
+    setOutputText(data.choices[0].text);
+    setInputText('');
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <header>Fun with AI</header>
+      <form onSubmit={onSubmit}>
+        <label>
+          Enter prompt
+          <input type='text' value={inputText} onChange={updateInputText}></input>
+        </label>
+        <input type='submit' value='Submit' />
+      </form>
+      <section>
+        Result: {outputText}
+      </section>
     </div>
   );
-}
+};
 
 export default App;
